c14e01be6c2a8b647c413598758a55ce4e84f4b3
dapowazk <132092451+dapowazk@users.noreply.github.com>
2023-05-01 15:26:06 +0200

    Create remove_user_from_group.bash

---
+++ bash/remove_user_from_group.bash
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+
+#Damian Powazka
+
+
+
+echo "Removing user from group"
+
+
+
+echo "Enter username: "
+
+read userd
+
+
+
+if id "$userd" >/dev/null 2>&1; then
+
+  echo "Enter group: "
+
+  read groupd
+
+
+
+  if groups $userd | grep $groupd >/dev/null 2>&1; then
+
+    echo "$userd $groupd"
+
+
+
+    gpasswd -d $userd $groupd
+
+  else
+
+    echo "User does not belong to this group"
+
+  fi
+
+
+
+else
+
+  echo "User $userd does not exist"
+
+fi
