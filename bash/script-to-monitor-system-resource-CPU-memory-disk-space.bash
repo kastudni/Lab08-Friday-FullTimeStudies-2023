24d728402dee3ef394b0ca13d6b6124fe1b58f6e
dawidew <76003346+dawidew@users.noreply.github.com>
2023-05-06 17:16:45 +0200

    Delete script-to-monitor-system-resource-CPU-memory-disk-space.bash

--- bash/script-to-monitor-system-resource-CPU-memory-disk-space.bash
+++
@@ -1,9 +0,0 @@
-# Made by Dawid Wolkowicz
-echo "CPU usage:"
-top -bn1 | grep load | awk '{printf "%.2f\n", $(NF-2)}'
-
-echo "Memory usage:"
-free -m | awk 'NR==2{printf "%.2f%%\n", $3*100/$2}'
-
-echo "Disk space usage:"
-df -h | awk '$NF=="/"{printf "%s\n", $5}'
