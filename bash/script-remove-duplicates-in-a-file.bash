253eb1f228a2d55e492a42f8b498825145b39a0d
CookieDM <cookiepl98@gmail.com>
2023-05-07 23:05:56 +0200

    script-remove-duplicates-in-a-file.bash

--- bash/script-remove-duplicates-in-a-file.bash
+++ bash/script-remove-duplicates-in-a-file.bash
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-<<<<<<< HEAD
 #Filip Bednarczyk
 
 
@@ -21,22 +19,3 @@ sort -u "$filename" > "$temp_file"
 mv "$temp_file" "$filename"
 
 echo "Duplicates removed from $filename"
-=======
-if [ -z "$1" ]; then
-    echo "Usage: $0 <filename>"
-    exit 1
-fi
-
-if [ ! -f "$1" ]; then
-    echo "$1 is not a file"
-    exit 1
-fi
-
-temp_file=$(mktemp)
-
-sort -u "$1" > "$temp_file"
-
-mv "$temp_file" "$1"
-
-echo "Duplicates removed from $1"
->>>>>>> 019e276ea15607d51b697b8c5ce0c6d5b873a48c
