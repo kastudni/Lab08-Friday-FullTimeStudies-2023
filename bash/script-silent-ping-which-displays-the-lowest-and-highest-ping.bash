bfc9785682e672ef267581ceaf073e97a4eb2fc9
canto_mordko <68858501+cant0@users.noreply.github.com>
2023-05-07 20:52:03 +0200

    Update script-silent-ping-which-displays-the-lowest-and-highest-ping.bash

--- bash/script-silent-ping-which-displays-the-lowest-and-highest-ping.bash
+++ bash/script-silent-ping-which-displays-the-lowest-and-highest-ping.bash
@@ -1,28 +1,13 @@
 #!/bin/bash
 
-# Wiktor Piotrowski
+address="google.com"
 
-silent_ping() {
-    ping -c 1 -W 1 -q "$1" &> /dev/null
-    return $?
-}
-
-get_ping_times() {
-    local host="$1"
-    local count="$2"
-    local times=$(ping -c "$count" "$host" | grep "time=" | cut -d "=" -f 4)
-    echo "$times"
-}
-
-host="google.com"
 count=5
 
-if silent_ping "$host"; then
-    times=($(get_ping_times "$host" "$count"))
-    min_time=$(printf "%s\n" "${times[@]}" | sort -n | head -n 1)
-    max_time=$(printf "%s\n" "${times[@]}" | sort -n | tail -n 1)
-    echo "Lowest ping time: $min_time ms"
-    echo "Highest ping time: $max_time ms"
-else
-    echo "$host is not reachable"
-fi
+output=$(ping -q -c $count $address)
+
+min_ping=$(echo "$output" | grep "min/avg/max" | awk '{print $4}' | cut -d '/' -f 1)
+max_ping=$(echo "$output" | grep "min/avg/max" | awk '{print $4}' | cut -d '/' -f 3)
+
+echo "Najniższy czas odpowiedzi: $min_ping ms"
+echo "Najwyższy czas odpowiedzi: $max_ping ms"
