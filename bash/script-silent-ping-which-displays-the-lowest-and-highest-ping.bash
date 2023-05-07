ef2790306d318a6196d5f913a177ca7225d0577b
canto_mordko <68858501+cant0@users.noreply.github.com>
2023-05-07 21:41:44 +0200

    Update script-silent-ping-which-displays-the-lowest-and-highest-ping.bash

--- bash/script-silent-ping-which-displays-the-lowest-and-highest-ping.bash
+++ bash/script-silent-ping-which-displays-the-lowest-and-highest-ping.bash
@@ -9,5 +9,5 @@ output=$(ping -q -c $count $address)
 min_ping=$(echo "$output" | grep "min/avg/max" | awk '{print $4}' | cut -d '/' -f 1)
 max_ping=$(echo "$output" | grep "min/avg/max" | awk '{print $4}' | cut -d '/' -f 3)
 
-echo "Najniższy czas odpowiedzi: $min_ping ms"
-echo "Najwyższy czas odpowiedzi: $max_ping ms"
+echo "Lowest ping time: $min_ping ms"
+echo "Highest ping time: $max_ping ms"
