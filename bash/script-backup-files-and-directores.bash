4bd57b04113ea15d0b7a30a5e4a8e7debe8ee60f
LVVL2 <lipnitsk05@gmail.com>
2023-05-11 18:15:04 +0200

    update

--- bash/script-backup-files-and-directores.bash
+++ bash/script-backup-files-and-directores.bash
@@ -2,20 +2,19 @@
 
 #Vladyslav Lipnitskyi
 
-# Remote server details
-remote_user="your_remote_hostname"
-remote_host="your_remote_hostname"
-remote_directory="/path/to/remote/directory/"
+# Get current date and time for naming backup folder
+date=$(date +"%Y-%m-%d_%H-%M-%S")
 
-# Source files/directories to backup
-source_files=(
-    "/path/to/source/file1"
-    "/path/to/source/file2"
-    "/path/to/source/directory/"
-)
+# Set your source and destination directories
+read -p "Enter the path to the source directory: " source_dir
+read -p "Enter the remote server address in the format user@remote.host:/path/to/destination: " destination_dir
 
-# Perform backup using rsync
-rsync_options="-avz --progress"
-for source in "${source_files[@]}"; do
-    rsync $rsync_options "$source" "$remote_user@$remote_host:$remote_directory"
-done
+# Run the rsync command to backup files and directories
+rsync -avz --delete $source_dir $destination_dir/$date
+
+# Check the rsync command status
+if [ $? -eq 0 ]; then
+  echo "Backup completed successfully"
+else
+  echo "Backup failed"
+fi
